8f8d61d3a64775ac7b24f0f5cb753cf29e58b75b
Chris Bogdon <chris@bogdonfamily.com>
2020-04-12 07:29:54 -0400

    Cleaned up the utils.py file for pep8

--- app/utils.py
+++ app/utils.py
@@ -2,21 +2,30 @@ import time
 import logging
 
 
-
 def generate_uniqueID():
 
-    '''
+    """
     This function will generate a uniqueID for indexes on the data base.   It has nanosecond granularity to ensure
     the the function will always be unique
     :return: unique value
     :rtype: integer
-    '''
+    """
     logging.debug("Entering generate_uniqueID")
     value = time.strftime("%Y%j%H%M") + str(time.process_time_ns() // 1000)
     logging.debug(f"Unique ID='{value}'")
     return value
 
+
 def cleanup_songs(songs):
+
+    """
+    This function will take a list of songs and then attempt to clean up the list.
+    For example, removing leading spaces, removing trailing spaces, removing any blank albums.
+    :param songs: list of songs to clean up
+    :type songs: list
+    :return: updated songs
+    :rtype: list
+    """
 
     logging.debug(f"Entering cleanup_songs")
     logging.debug(f"Song list to be cleaned up: {songs}")
@@ -29,10 +38,9 @@ def cleanup_songs(songs):
             blanks.append(i)
         count += 1
 
-    # Noe let's remove the blank charaters
+    # Now let's remove the blank characters
     for i in blanks:
         songs.remove(i)
-
 
     final_songs = []
     for i in songs:
@@ -40,5 +48,4 @@ def cleanup_songs(songs):
 
     logging.debug(f"Song list after cleaned up:{final_songs}")
 
-    return (final_songs)
-
+    return final_songs
