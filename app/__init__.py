e930acfe52c2ed71c16379fc75ac0ea10f87c7fc
Chris Bogdon <chris@bogdonfamily.com>
2020-04-13 07:08:14 -0400

    Added a RotatingFileHandler in addition to the stream handling

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 import logging
+from logging.handlers import RotatingFileHandler
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -25,10 +26,15 @@ version = ".9b"
 # Create a global application configuration object
 musicdb_config = AppConfig()
 
+logging.getLogger()
+handler = RotatingFileHandler("musicdb.log",maxBytes=1000000,backupCount=5)
+logging.basicConfig(level=logging.INFO, format='%(asctime)-15s-%(funcName)-15s %(levelname)-8s %(message)s', \
+                    handlers=[handler,logging.StreamHandler()])
 if musicdb_config.getitem("LOGGING") == 'DEBUG':
     logging.getLogger().setLevel(logging.DEBUG)
 else:
     logging.getLogger().setLevel(logging.INFO)
+
 
 
 from app import routes, models, errors
--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
-    logging.basicConfig(level=logging.INFO,format='%(asctime)-15s-%(funcName)-15s %(levelname)-8s %(message)s')
+
 #from app.models import Configuration
 
 
